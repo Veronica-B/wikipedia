72a60ed045179b18cfbce946893c187e3f5760b8
anaruizrivera <anaruizrivera91@icloud.com>
2020-06-24 16:07:49 -0400

    adding home page

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import '../css/footer.css'
 
 function Footer() {
+    return (
+        <React.Fragment>
+            <footer>
+                <div className="footer-container">
+                <h5>Wikipedia</h5>
+                <p>Wikipedia is a multilingual online encyclopedia created and 
+                    maintained as an open collaboration project by a community 
+                    of volunteer editors using a wiki-based editing system. It 
+                    is the largest and most popular general reference work on 
+                    the World Wide Web.</p><br/>
+                    </div>
+
+
+            </footer>
+        </React.Fragment>
+    )
 }
 
 
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import Footer from '../components/footer'
+import Footer from '../components/footer';
+import '../css/home.css'
 
 
-function Home (){
+function Home() {
     return (
-<div> Home</div>
+        <React.Fragment>
+            <div>
+                <div className="initial_container">
+                    <h1 className="logo_firstname">WIKIPEDIA<br /><h4>The Free Encyclopedia</h4></h1>
+
+                    <img className="wikipedia_logo" src="https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1200px-Wikipedia-logo-v2.svg.png" />
+                </div>
+                <section>
+    <div className="ui action input">
+      <input type="text" placeholder="Search Wikipedia..." class="search" id="input-search" />
+      <button className="search ui button btn-wiki" id="button-search">Search</button>
+      </div>
+  
+  </section>
+                    
+                    
+                   
+            </div>
+
+        </React.Fragment>
     )
-} 
+}
 
 export default Home;
