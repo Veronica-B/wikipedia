4063e147825bd7733df164578e1787243648710d
Adrianna-IO <isom.adrianna@gmail.com>
2020-06-25 17:53:12 -0400

    pushed up more changes to sephora page

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -11,12 +11,8 @@ function Home() {
 
                     <img className="wikipedia_logo" src="https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1200px-Wikipedia-logo-v2.svg.png" />
                     <div className="ui action input">
-<<<<<<< HEAD
-                        <input type="text" placeholder="Search Wikipedia..." className="search" id="input-search" />
-=======
                     <form action="http://localhost:3000/sephora" target= "_blank" method="GET">
                         <input type="text" placeholder="Search Wikipedia..." class="search" id="input-search" />
->>>>>>> 6d1fe845cb42b2dc45a1e400051c780a12fd6d13
                         <button className="search ui button btn-wiki" id="button-search">Search</button>
                         </form>
                     </div>
