42efb190ce9e9cf081ad3435938024c85f62ab5c
Veronica Barcia <veronicabarcia4@gmail.com>
2020-07-14 17:13:16 -0400

    rename

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const cors = require('cors')
 const app = express();
 
 
-app.use(bodyParser.urlencoded({extended: false}))
-let router =require('./Routes/apiContact')
-let router_v =require('./Routes/apiVerification')
-let router_a =require('./Routes/apiArticle')
+app.use(bodyParser.urlencoded({ extended: false }))
+let router = require('./Routes/apiContact')
+let router_v = require('./Routes/apiVerification')
+let router_a = require('./Routes/apiArticle')
 require('./node_modules/dotenv/types').config();
 app.use(express.json()); // turns my app into a json 
 app.use(cors());
@@ -17,14 +17,14 @@ app.use(cors());
 
 // Server static assets in production 
 app.use(express.static('client/build'));
-app.get("*", (req,res) =>{
-  res.sendFile(path.resolve(__dirname,  "./client/build", "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 
 //sets server up
-app.listen(process.env.PORTSERVER || 3000, () => {
-  console.log(`Server running on port 8080`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 })
 
 
