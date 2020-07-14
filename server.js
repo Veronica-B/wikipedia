bce4c49bcaa498b08a75848cb26582f0421bf1ee
Veronica Barcia <veronicabarcia4@gmail.com>
2020-07-14 18:47:44 -0400

    help

--- server.js
+++ server.js
@@ -17,20 +17,21 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
-/* let router = require('./Routes/apiContact')
+// Messing Up The Huroku App 
+ let router = require('./Routes/apiContact')
 let router_v = require('./Routes/apiVerification')
 let router_a = require('./Routes/apiArticle')
 app.use('/api', router);
 app.use('/api', router_v);
 app.use('/api', router_a);
-app.use('/', require('./Routes/rootApi'));
-*/
+// app.use('/', require('./Routes/rootApi'));
+
 
 
 
 
 
 //sets server up
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(process.env.PORTSERVER || 3000, () => {
+  console.log(`Server running on port ${process.env.PORTSERVER}`);
 })
