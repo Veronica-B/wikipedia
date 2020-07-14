cf303b7ee44d9cf66c540de63702a416c283c1b6
Veronica Barcia <veronicabarcia4@gmail.com>
2020-07-14 17:36:49 -0400

    adding stuff

--- server.js
+++ server.js
@@ -4,13 +4,9 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const cors = require('cors')
 const app = express();
-
-
+const dotenv = require('dotenv')
+dotenv.config();
 app.use(bodyParser.urlencoded({ extended: false }))
-let router = require('./Routes/apiContact')
-let router_v = require('./Routes/apiVerification')
-let router_a = require('./Routes/apiArticle')
-require('./node_modules/dotenv/types').config();
 app.use(express.json()); // turns my app into a json 
 app.use(cors());
 
@@ -21,15 +17,17 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
-
-//sets server up
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-})
-
+let router = require('./Routes/apiContact')
+let router_v = require('./Routes/apiVerification')
+let router_a = require('./Routes/apiArticle')
 
 app.use('/api', router);
 app.use('/api', router_v);
 app.use('/api', router_a);
 
 
+
+//sets server up
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+})
